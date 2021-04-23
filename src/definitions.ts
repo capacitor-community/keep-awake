1a44d54e66f3d18e40f052fde402ddd685fe3dec
Robin Genz <mail@robingenz.dev>
2021-04-23 22:04:37 +0200

    feat: add `web` support

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,14 +1,10 @@
 export interface KeepAwakePlugin {
   /**
    * Prevent the device from dimming the screen.
-   *
-   * Only available for Android and iOS.
    */
   keepAwake(): Promise<void>;
   /**
    * Allow the device to dim the screen.
-   *
-   * Only available for Android and iOS.
    */
   allowSleep(): Promise<void>;
 }
