ad63ddc1d427f69e6d4b4612ab9c7ce08fab5418
Robin Genz <mail@robingenz.dev>
2025-12-20 14:49:25 +0100

    feat!: update to Capacitor 8
    BREAKING CHANGE: This plugin now supports Capacitor 8.

--- Package.swift
+++ Package.swift
@@ -3,14 +3,14 @@ import PackageDescription
 
 let package = Package(
     name: "CapacitorCommunityKeepAwake",
-    platforms: [.iOS(.v14)],
+    platforms: [.iOS(.v15)],
     products: [
         .library(
             name: "CapacitorCommunityKeepAwake",
             targets: ["KeepAwakePlugin"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "7.0.0")
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0")
     ],
     targets: [
         .target(
